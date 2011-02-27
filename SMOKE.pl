1e2f9b08d38dfe84b2251bae8f218e9853e509f8
Ashley Pond V <apv@cassidy.local>
2011-02-26 16:15:47 -0800

    Clean-up a bit, unlocalized repo.

--- SMOKE.pl
+++ SMOKE.pl
@@ -2,18 +2,17 @@
 use warnings;
 use strict;
 use File::Temp qw( tempdir );
-use Capture::Tiny qw( capture_merged tee );
+use Capture::Tiny qw( capture_merged );
 use App::Prove;
 
 my $dir = tempdir( CLEANUP => 1 );
 -d $dir or die "Temp build dir created";
 chdir $dir or die "Couldn't chdir $dir";
 
-#    my $clone = capture_merged { system qw( git clone git://github.com/pangyre/p5-elektrum.git ) };
-#    $clone =~ qr/Initialized empty Git repository/
-#        or die "Git repo failed: $clone";
-#    my $app_dir = "p5-elektrum";
-my $app_dir = "/Users/apv/depot/p5-elektrum";
+my $clone = capture_merged { system qw( git clone git://github.com/pangyre/p5-elektrum.git ) };
+$clone =~ qr/Initialized empty Git repository/
+    or die "Git repo failed: $clone";
+my $app_dir = "p5-elektrum";
 chdir $app_dir or die "Couldn't chdir $app_dir";
 
 my @smoke_args = qw( -l t/ -r );
