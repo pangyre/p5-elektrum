adca0d56b7aa0fbae2946c07532b85643ecc320c
Ashley Pond V <apv@cassidy.local>
2011-02-26 16:06:26 -0800

    Clean-up a bit.

--- SMOKE.pl
+++ SMOKE.pl
@@ -9,36 +9,32 @@ my $dir = tempdir( CLEANUP => 1 );
 -d $dir or die "Temp build dir created";
 chdir $dir or die "Couldn't chdir $dir";
 
-#    my $clone = capture_merged { system qw( git clone git@github.com:pangyre/p5-elektrum.git ) };
+#    my $clone = capture_merged { system qw( git clone git://github.com/pangyre/p5-elektrum.git ) };
 #    $clone =~ qr/Initialized empty Git repository/
 #        or die "Git repo failed: $clone";
 #    my $app_dir = "p5-elektrum";
 my $app_dir = "/Users/apv/depot/p5-elektrum";
 chdir $app_dir or die "Couldn't chdir $app_dir";
 
-
 my @smoke_args = qw( -l t/ -r );
 push @smoke_args, "-v" if $ENV{TEST_VERBOSE};
 
-print "Running application tests --------------------------\n"
-    if -t STDOUT;
-
-# Not yet local $ENV{TEST_POD} = 1;
+# Not yet, local $ENV{TEST_POD} = 1;
 
 my $app = App::Prove->new;
 $app->process_args( @smoke_args );
 my $result = $app->run;
 
-# system "prove", @smoke_args;
-
-print "Done with application tests ------------------------\n"
-    if -t STDOUT;
-
 chdir or warn "Couldn't chdir for safe cleanup of tempdir\n";
 
 exit 0;
 
-#  Subroutines
-#---------------------------------------------------------------------
-
 __DATA__
+
+=pod
+
+=head1 ...
+
+  env TEST_VERBOSE=1 perl -Ilib SMOKE.pl
+
+=cut
