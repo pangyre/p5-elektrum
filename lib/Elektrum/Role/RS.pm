a32923fc1236e0138c7cd829a34024a31b1e752d
Ashley Pond V <ashley@cpan.org>
2011-09-20 17:33:55 -0700

    Cleanup of role.

--- lib/Elektrum/Role/RS.pm
+++ lib/Elektrum/Role/RS.pm
@@ -1,24 +1,30 @@
 package Elektrum::Role::RS;
 use Moose::Role;
-requires qw( base );
+use namespace::autoclean;
+requires "base";
 
 has "rs" =>
     is => "ro",
+    isa => "Object",
     writer => "set_rs",
     ;
 
 has "model" =>
     is => "ro",
+    isa => "Str|Object",
+    predicate => "has_model",
     writer => "set_model",
     ;
 
 after "base" => sub {
     my ( $self, $c ) = @_;
     my $class = blessed($self);
-    ( my $model = $class ) =~ s/\A.+::/DBIC::/; # E.g., Elektrum::Controller::Node
-    ( my $isa = $class ) =~ s/Controller/Schema::ResultSet/;
-    $self->set_rs( $c->model($model)->search_rs );
-    $self->set_model( $c->model($model) );
+    unless ( $self->has_model )
+    {
+        ( my $model = $class ) =~ s/\A.+::/DBIC::/; # E.g., Elektrum::Controller::Node
+        $self->set_model( $model );
+    }
+    $self->set_rs( $c->model( $self->model )->search_rs );
 };
 
 sub txn_scope_guard {
